35024b6db4b41b7688193b938164c976aa131490
GoodYes <wanjun8888@gmail.com>
2019-04-21 11:47:33 +0800

    Update sk_client_api.sh

--- sk_client_api.sh
+++ sk_client_api.sh
@@ -1,1 +1,1 @@
-["http:\/\/client_api.systempanel.net","https:\/\/client_api.systempanel.net"]
+{"code":20000,"message":"success","date":"2019-04-20 11:47:00","data":["http:\/\/client_api.systempanel.net","https:\/\/client_api.systempanel.net"]}
